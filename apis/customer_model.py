223313564cbecdf2da34e705008a8dbb3e517123
amirali <amirali.safie@gamil.com>
2025-09-17 01:29:09 +0200

    change apis and form to sync with eachother

--- apis/customer_model.py
+++ apis/customer_model.py
@@ -5,5 +5,6 @@ class CustomerInfo(BaseModel):
     l_name: str | None = None
     national_code: str | None = None
     city: str | None = None
+    plan_type: str | None = None
 
 
--- apis/provide_sim.py
+++ apis/provide_sim.py
@@ -6,21 +6,28 @@ app = FastAPI(title="sim card provision apis")
 @app.post('/info-validation')
 def validate_customer_info(customer_info: CustomerInfo):
     """
-    get customer data and validate it and return a result as bool
+    Get customer data and validate it with a cleaner manual approach.
     """
-    
     customer_info_dict = customer_info.model_dump()
-
-    if not (customer_info_dict.get("f_name") and customer_info_dict.get("l_name") and customer_info_dict.get("city")):
-        raise HTTPException(status_code=400, detail="Invalid customer info")
-
-
-    if customer_info_dict.get("national_code"):
-        if len(customer_info_dict.get("national_code")) != 10:
-            raise HTTPException(status_code=400, detail="Invalid customer info")
+    required_fields = {"f_name", "l_name", "city", "plan_type", "national_code"}
     
-    return {"success": True}
-        
+    
+    for field in required_fields:
+        if not customer_info_dict.get(field):
+            print("bad request for field ",field)
+            raise HTTPException(status_code=400, detail="Invalid customer info: Missing field")
+
+   
+    national_code = customer_info_dict.get("national_code")
+    plan_type = customer_info_dict.get("plan_type")
+    print(f'reccied :::: {customer_info_dict}')
+    if len(national_code) != 10:
+        return {"isValidCustomer": False, "message":"national code is invalid"}
+
+    if plan_type not in ("D", "E"):
+        return {"isValidCustomer": False, "message":"plan type undifined"}
+    
+    return {"isValidCustomer": True}
 
 
 @app.post('/customer-info')
@@ -33,7 +40,7 @@ def store_customer_info(customer_info: CustomerInfo):
 
     pass
     
-    return {"success": True}
+    return {"isValidCustomer": True}
         
 
 
@@ -47,7 +54,7 @@ def calculate_bill(customer_info: CustomerInfo):
 
     pass
     
-    return {"success": True}
+    return {"isValidCustomer": True}
         
 
 @app.post('/store-bill')
@@ -60,5 +67,22 @@ def store_bill(customer_info: CustomerInfo):
 
     pass
     
-    return {"success": True}
+    return {"isValidCustomer": True}
         
+
+
+
+# {
+#   "status": 200,
+#   "headers": {
+#     "date": "Tue, 16 Sep 2025 22:31:36 GMT",
+#     "content-length": "24",
+#     "server": "uvicorn",
+#     "content-type": "application/json"
+#   },
+#   "body": {
+#     "isValidCustomer": true
+#   },
+#   "reason": "OK",
+#   "document": null
+# }
