cc709cd644908d37195141dacfc05bf31446fc66
Fatemeh Khavari <138055099+FatemehKhavari@users.noreply.github.com>
2025-09-22 21:18:14 +0330

    Billing related models added.

--- apis/customer_model.py
+++ apis/customer_model.py
@@ -11,3 +11,16 @@ class CustomerInfo(BaseModel):
 class PhoneNumber(BaseModel):
     national_code: str | None = None
     phone_number: str | None = None
+
+
+class BillingRequest(BaseModel):
+    national_code: str | None = None
+    plan_type: str | None = None
+    phone_number: str | None = None
+
+class BillingInfo(BaseModel):
+    national_code: str | None = None
+    plan_type: str | None = None
+    phone_number: str | None = None
+    total: int | None = None
+    bill_id: str | None = None
